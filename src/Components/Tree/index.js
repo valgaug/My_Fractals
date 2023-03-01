f6bcbf5dff979301efc7e033f8bdc996e4108bd3
valgaug <valentin.gaugain@hotmail.fr>
2023-03-01 19:21:49 +0100

    fix: tree working

--- src/Components/Tree/index.js
+++ src/Components/Tree/index.js
@@ -10,11 +10,36 @@ function Tree() {
   };
 
   const draw = (p5) => {
+    const branch = (h) => {
+      // Each branch will be 2/3rds the size of the previous one
+      h *= 0.66;
+
+      // All recursive functions must have an exit condition!!!!
+      // Here, ours is when the length of the branch is 2 pixels or less
+      if (h > 2) {
+        p5.push(); // Save the current state of transformation (i.e. where are we now)
+        p5.rotate(theta); // Rotate by theta
+        p5.line(0, 0, 0, -h); // Draw the branch
+        p5.translate(0, -h); // Move to the end of the branch
+        branch(h); // Ok, now call myself to draw two new branches!!
+        p5.pop(); // Whenever we get back here, we "pop" in order to restore the previous matrix state
+
+        // Repeat the same thing, only branch off to the "left" this time!
+        p5.push();
+        p5.rotate(-theta);
+        p5.line(0, 0, 0, -h);
+        p5.translate(0, -h);
+        branch(h);
+        p5.pop();
+      }
+    };
+
     p5.background(0);
     p5.frameRate(30);
     p5.stroke(255);
     // Let's pick an angle 0 to 90 degrees based on the mouse position
     let a = (p5.mouseX / p5.width) * 90;
+    // let a = 45;
     // Convert it to radians
     theta = p5.radians(a);
     // Start the tree from the bottom of the screen
@@ -25,30 +50,6 @@ function Tree() {
     p5.translate(0, -120);
     // Start the recursive branching!
     branch(120);
-  };
-
-  const branch = (h, p5) => {
-    // Each branch will be 2/3rds the size of the previous one
-    h *= 0.66;
-
-    // All recursive functions must have an exit condition!!!!
-    // Here, ours is when the length of the branch is 2 pixels or less
-    if (h > 2) {
-      p5.push(); // Save the current state of transformation (i.e. where are we now)
-      p5.rotate(theta); // Rotate by theta
-      p5.line(0, 0, 0, -h); // Draw the branch
-      p5.translate(0, -h); // Move to the end of the branch
-      p5.branch(h); // Ok, now call myself to draw two new branches!!
-      p5.pop(); // Whenever we get back here, we "pop" in order to restore the previous matrix state
-
-      // Repeat the same thing, only branch off to the "left" this time!
-      p5.push();
-      p5.rotate(-theta);
-      p5.line(0, 0, 0, -h);
-      p5.translate(0, -h);
-      p5.branch(h);
-      p5.pop();
-    }
   };
 
   return (
