30f4dd3d38b560e4ccbc43a8d4b145e641ff9186
valgaug <valentin.gaugain@hotmail.fr>
2023-03-07 21:25:13 +0100

    fix: last commit

--- client/src/Components/04-03-Mandelbrot/index.js
+++ client/src/Components/04-03-Mandelbrot/index.js
@@ -3,23 +3,19 @@ import Sketch from 'react-p5';
 import './style.css';
 import canvasToImage from 'canvas-to-image';
 import * as ApiService from '../../ApiService';
+// import xpic from '../../assets/keyboard_key_x.png';
+// import zpic from '../../assets/keyboard_key_z.png';
+// import arrowpic from '../../assets/arrows_keys.png';
 
 function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
-  // const [maxIterations, setMaxIterations] = useState(100);
-  // const [param1, setParam1] = useState(66);
-  // const [param2, setParam2] = useState(42);
-  // const [param3, setParam3] = useState(115);
-  // const [param4, setParam4] = useState(342);
-  // const [param5, setParam5] = useState(378);
-
+  const [maxIterations, setMaxIterations] = useState(30);
+  const [param1, setParam1] = useState(0.5);
+  const [param2, setParam2] = useState(166);
+  const [param3, setParam3] = useState(11);
   const canvasRef = useRef(null);
-
-  // const [slider, setSlider] = useState(100);
 
   const setup = (p5, canvasParentRef) => {
     p5.createCanvas(730, 520).parent(canvasParentRef);
-    // p5.pixelDensity(1);
-    p5.noStroke();
     canvasRef.current = p5.canvas;
   };
 
@@ -34,8 +30,8 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
 
     const pixelToPoint = (x, y) => {
       let p = p5.createVector(
-        (x - p5.width / 2) * (4 / p5.width) * (73 / (52 * scale)) + cenX,
-        (y - p5.height / 2) * (4 / p5.height) * (1 / scale) + cenY
+        (x - p5.width / 1.53) * (2.5 / p5.width) * (73 / (52 * scale)) + cenX,
+        (y - p5.height / 2) * (2.5 / p5.height) * (1 / scale) + cenY
       );
       return p;
     };
@@ -45,7 +41,7 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
       let i = 0;
       let bounds = 2;
       let isIn = true;
-      while (i < 50 && isIn) {
+      while (i < maxIterations && isIn) {
         z0 = p5.createVector(
           z0.x * z0.x - z0.y * z0.y + c.x,
           2 * z0.x * z0.y + c.y
@@ -68,18 +64,18 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
           let result = calculatePoint(c);
           if (result.isIn) {
             p5.set(x, y, p5.color(0));
-          } else if (result.i > 1) {
+          } else if (result.i > param3) {
             p5.set(
               x,
               y,
               p5.color(
-                150 + 200 - ((p5.pow(result.i / 50, 0.5) * 200) % 255),
+                param2 - ((p5.pow(result.i / 50, param1) * 200) % 255),
                 80,
                 100
               )
             );
           } else {
-            p5.set(x, y, p5.color(50));
+            p5.set(x, y, p5.color(0));
           }
         }
       }
@@ -136,7 +132,7 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
   return (
     <div className='mandelbrot'>
       <Sketch setup={setup} draw={draw} />
-      {/* <form>
+      <form>
         <div className='parameter'>
           <span>Iterations:</span>
           <label>{maxIterations}</label>
@@ -145,8 +141,8 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
             name='mandelbrot'
             id='iteration'
             min='0'
-            max='200'
-            step='5'
+            max='100'
+            step='1'
             value={maxIterations}
             onChange={(e) => {
               setMaxIterations(e.target.value);
@@ -154,15 +150,15 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
           ></input>
         </div>
         <div className='parameter'>
-          <span>HSB range:</span>
+          <span>HSB color:</span>
           <label>{param1}</label>
           <input
             type='range'
             name='mandelbrot'
             id='HSB_range'
             min='0'
-            max='100'
-            step='1'
+            max='1'
+            step='0.01'
             value={param1}
             onChange={(e) => {
               setParam1(e.target.value);
@@ -171,14 +167,14 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
           <br />
         </div>
         <div className='parameter'>
-          <span>Hue start:</span>
+          <span>Hue:</span>
           <label>{param2}</label>
           <input
             type='range'
             name='mandelbrot'
-            id='param1'
+            id='param2'
             min='0'
-            max='200'
+            max='360'
             step='1'
             value={param2}
             onChange={(e) => {
@@ -188,15 +184,15 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
           <br />
         </div>
         <div className='parameter'>
-          <span>Hue end:</span>
+          <span>Off limit:</span>
           <label>{param3}</label>
           <input
             type='range'
-            name='mandelbrot'
+            name='mandlebrot'
             id='param1'
             min='0'
-            max='200'
-            step='1'
+            max='17'
+            step='0.1'
             value={param3}
             onChange={(e) => {
               setParam3(e.target.value);
@@ -204,41 +200,46 @@ function Mandelbrot({ hideMandelbrot, setHideMandelbrot, post, setPost }) {
           ></input>
           <br />
         </div>
-        <div className='parameter'>
-          <span>Brightness start:</span>
-          <label>{param4}</label>
-          <input
-            type='range'
-            name='mandlebrot'
-            id='param1'
-            min='0'
-            max='800'
-            step='1'
-            value={param4}
-            onChange={(e) => {
-              setParam4(e.target.value);
-            }}
-          ></input>
+        <div className='keys'>
+          <div className='parameter'>
+            <span>Zoom in:</span>
+            <label>Z key</label>
+            <br />
+          </div>
+          <div className='parameter'>
+            <span>Zoom out:</span>
+            <label>X key</label>
+            <br />
+          </div>
+          <div className='parameter'>
+            <span>Move:</span>
+            <label>Arrow keys</label>
+            <br />
+          </div>
+        </div>
+        {/* <div className='parameter'>
+          <span>Zoom in:</span>
+          <label>
+            Z key
+            <img className='keypic' src={zpic} alt=''></img>
+          </label>
           <br />
         </div>
-        <div className='parameter'>
-          <span>Brightness end:</span>
-          <label>{param5}</label>
-          <input
-            type='range'
-            name='mandelbrot'
-            id='param1'
-            min='0'
-            max='800'
-            step='1'
-            value={param5}
-            onChange={(e) => {
-              setParam5(e.target.value);
-            }}
-          ></input>
+        <div className='key'>
+          <span>Zoom out:</span>
+          <label>
+            <img src={xpic} alt=''></img>
+          </label>
           <br />
         </div>
-      </form> */}
+        <div className='key'>
+          <span>Move:</span>
+          <label>
+            <img src={arrowpic} alt=''></img>
+          </label>
+          <br />
+        </div> */}
+      </form>
       <div className='buttons'>
         <button onClick={postCanvasAsImage}>Submit</button>
         <button onClick={saveCanvasAsImage}>Download</button>
