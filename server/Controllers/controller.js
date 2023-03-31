967a81c8bbd84ca1c46c414b101c0c618bbba4bb
valgaug <valentin.gaugain@hotmail.fr>
2023-03-31 18:08:08 +0200

    try5

--- server/Controllers/controller.js
+++ server/Controllers/controller.js
@@ -7,9 +7,9 @@ const path = require('path');
 exports.getImages = async (req, res) => {
   try {
     const buffer_images = await image.find();
-    res.json({ message: 'hello from get images' });
-    // const images = buffer_images.map((img) => Buffer.from(img.data, 'base64').toString('base64'));
-    // res.send(images);
+    // res.json({ message: 'hello from get images' });
+    const images = buffer_images.map((img) => Buffer.from(img.data, 'base64').toString('base64'));
+    res.send(images);
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
