5e3f9851348691001d0cfe4ebe475c1129ba08df
valgaug <valentin.gaugain@hotmail.fr>
2023-04-03 10:51:56 +0200

    new MongoDB connection

--- server/Models/index.js
+++ server/Models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const db = `mongodb+srv://valgaug:myfract74@myfractals.so9obsd.mongodb.net/?retryWrites=true&w=majority`;
+const db = process.env.MONGODB_URI;
+// const db = `mongodb+srv://valgaug:myfract74@myfractals.so9obsd.mongodb.net/?retryWrites=true&w=majority`;
 // const db = `mongodb+srv://valgaug:${process.env.DB_PASSWORD}@myfractals.so9obsd.mongodb.net/?retryWrites=true&w=majority`;
 // const db = 'mongodb://127.0.0.1:27017/imagesDB';
 
